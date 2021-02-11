ef487f1f3f774768ca94a511164ffafbb8d18ea9
Matthew French <mfrench71@googlemail.com>
2021-02-11 16:59:43 +0000

    Added missing base-component.js in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -88,6 +88,7 @@ module.exports = function (grunt) {
             "node_modules/bootstrap/js/dist/dom/event-handler.js",
             "node_modules/bootstrap/js/dist/dom/manipulator.js",
             "node_modules/bootstrap/js/dist/dom/selector-engine.js",
+            "node_modules/bootstrap/js/dist/base-component.js",
             "node_modules/bootstrap/js/dist/alert.js",
             "node_modules/bootstrap/js/dist/button.js",
             "node_modules/bootstrap/js/dist/carousel.js",
